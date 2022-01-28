105d948747dc4d0cc898a5e966a741d23b4efb00
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-01-28 01:09:52 -0800

    Create BuildMaxHeap.cpp

---
+++ DSA/DSA_PROBLEMS/Heap/BuildMaxHeap.cpp
@@ -0,0 +1,70 @@
+
+
+
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <math.h> 
+#include <algorithm> 
+
+using namespace std;
+void siftUp(int currenIndex,vector<int> &heap);
+void siftDown(int currentIdx,int endIdx,vector<int> &heap);
+void buildMaxHeapSiftUp(vector<int> &heap){
+    for(int i=0;i<heap.size();i++){
+        siftUp(i,heap);
+    }
+}
+void buildMaxHeapSiftDown(vector<int> &heap){
+    int firstparent = floor((heap.size()-2)/2);
+    for(int currentIdx=firstparent;currentIdx>=0;currentIdx--){
+        siftDown(currentIdx,heap.size()-1,heap);
+    }
+}
+
+void siftUp(int currentIdx,vector<int> &heap){
+    int parentIdx = floor((currentIdx - 1)/2);
+    while(currentIdx >0 && heap[currentIdx]>heap[parentIdx])
+    {
+        swap(heap[currentIdx],heap[parentIdx]);
+        currentIdx = parentIdx;
+        parentIdx = floor((currentIdx - 1)/2);
+    }
+}
+
+void siftDown(int currentIdx,int endIdx,vector<int> &heap)
+{
+    int childOne = currentIdx*2 + 1;
+    
+    while(childOne<=endIdx)
+    {
+        int childTwo = currentIdx*2 + 2 <= endIdx ? currentIdx*2 + 2 : -1;
+        int idxToSwap;
+        if(childTwo!=-1 && heap[childTwo] > heap[childOne])
+        {
+            idxToSwap = childTwo;
+        }
+        else
+       idxToSwap = childOne;
+    
+    if(heap[idxToSwap]>heap[currentIdx])
+    {
+        swap(heap[currentIdx],heap[idxToSwap]);
+        currentIdx = idxToSwap;
+       childOne = currentIdx*2 + 1;
+    }
+    else return;
+    }
+}
+int main()
+{
+    vector<int> heap{10,45,3,6,77,8};
+    buildMaxHeapSiftUp(heap);
+    for(auto i:heap)
+    cout<<i<<"\n";
+    cout<<"___________"<<endl;
+   buildMaxHeapSiftDown(heap);
+    for(auto i:heap)
+    cout<<i<<"\n";
+    return 0;
+}
