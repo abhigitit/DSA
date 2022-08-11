2a9317085feed6455e2383e173eb5102700f3bb1
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-08-10 23:27:06 -0700

    Update Text Justification.cpp

--- DSA/DSA_PROBLEMS/Strings/Text Justification.cpp
+++ DSA/DSA_PROBLEMS/Strings/Text Justification.cpp
@@ -1,4 +1,69 @@
 
+/*
+Leetcode Text Justification
 
+TC: O(lines*maxwidth)
 
-<img width="1133" alt="Screen Shot 2022-08-10 at 10 36 59 PM" src="https://user-images.githubusercontent.com/90536339/184070463-d5f7e89c-db03-4cb4-9fa3-cbfe45a64dc7.png">
+Intuition: https://www.youtube.com/watch?v=GqXlEbFVTXY&t=1414s&ab_channel=MichaelMuinos
+
+*/
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+       int i = 0;
+        int n = words.size();
+        vector<string> res;
+        while(i<n)
+        {
+            int j = i+1;
+            int line_len = words[i].size();
+            while(j<n and (line_len + words[j].size() + (j-i-1))<maxWidth)
+            {
+                line_len+= words[j].size();
+                j++;
+                
+            }
+            
+            int number_of_words = j-i;
+            int spaces = maxWidth - line_len;
+            if(number_of_words==1 or j>=n) res.push_back(leftJustified(i,j,spaces,words));
+            else
+                res.push_back(centerJustified(i,j,spaces,words));
+            i=j;
+        }
+        return res;
+    }
+    
+    string leftJustified(int i,int j, int spaces,vector<string>& words)
+    {
+        int spaces_in_between = j-i-1;
+        int spaces_on_right = spaces - spaces_in_between;
+        string temp = words[i];
+        for(int k=i+1;k<j;++k)
+        {
+            temp+= " " + words[k];
+        }
+        while(spaces_on_right--)
+        {
+            temp+=" ";
+        }
+        return temp;
+    }
+    
+    string centerJustified(int i,int j, int spaces,vector<string>& words)
+    {
+        int spaces_in_between = spaces/(j-i-1);
+        int extra_spaces = spaces%(j-i-1);
+        string temp = words[i];
+         for(int k=i+1;k<j;++k)
+        {
+             int spaces_to_add = spaces_in_between + (extra_spaces-- >0 ? 1 : 0);
+             while(spaces_to_add--)
+             {
+                 temp+= " ";
+             }
+            temp+=words[k];
+        }
+        return temp;
+    }
+};
