dd33c3b963c5b357872a30f770ac5f855db8e8eb
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-08-20 19:41:47 -0700

    Create Articulation Points.cpp

---
+++ DSA/DSA_PROBLEMS/Graphs/Articulation Points.cpp
@@ -0,0 +1,11 @@
+
+/*
+Articulation Points : A vertex is called an articulation point if rmeoving it and all edges associated with it, the number of
+connected components in the graph increases.
+
+Applications : to find critical points.
+
+Intuition: 
+
+*/
+
