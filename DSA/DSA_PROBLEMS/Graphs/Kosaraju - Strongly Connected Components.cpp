d500b4a170e14d0684150b1add9972497e84d210
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-08-21 21:44:23 -0700

    Create Kosaraju - Strongly Connected Components.cpp

---
+++ DSA/DSA_PROBLEMS/Graphs/Kosaraju - Strongly Connected Components.cpp
@@ -0,0 +1,8 @@
+/*
+
+Kosaraju's Algoithm to find Strongly Connected Components in directed graph.
+
+Strongly Connected Component : If we can reach from every vertex to every other vertx in a component of the graph, then the component 
+is called SSC.
+
+Single Node is always SSC.
