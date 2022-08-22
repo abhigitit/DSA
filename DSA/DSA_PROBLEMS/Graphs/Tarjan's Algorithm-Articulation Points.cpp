df4c0b6f11f2493b4b0ffb875f98d996a1843d75
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-08-21 22:17:27 -0700

    Update Tarjan's Algorithm-Articulation Points.cpp

--- DSA/DSA_PROBLEMS/Graphs/Tarjan's Algorithm-Articulation Points.cpp
+++ DSA/DSA_PROBLEMS/Graphs/Tarjan's Algorithm-Articulation Points.cpp
@@ -11,19 +11,27 @@ TC : O(V*(V+E))
 
 
 Better Algoritm: Tarjan's Algoritm
- In DFS tree, vertex u is articulation point if :
- 1) u is the root of DFS tree and it has atleast two children
- 2)u is not root of DFS tree  and it has a child v such that no vertex in the subtree of v is connected to ancestor of u using a backedge.(can have a backedge to u)
+ 1)Prepare DFS spanning tree.
+ 2)Mention discovery time d of each vertex in order of DFS traversal
+ 3)Mention low time L of each vertex which is the earliest ancestor current node can reach from it's child path. (Ancestor can be the direct parent also;
+ Once an ancestor is reached, no taking another edge to another ancestor)
+ 4)u is parent and v is child; if L[v]>=d[u], then u is an articulation point.[this does not hold true for root node]
+ 
+ The intuition here is, before discovering the "could be articulation node" at x time in the DFS spanning tree, we will have few nodes discovered with discovery time less than x.
+ If the child of "could be articulation node" has a backedge to an early ancestor whose discovery time is less than x, then the child of "could be articulation node" is given a low time y.
+y<x..
+
+So it means that if L[child] "y" < d[parent/ "could be articulation node"] "x" , then it means that even if could be articulation node is not present, we have this child
+linked with the ancestor of "could be articulation node". Hence the "could be articulation node" is not articulation. If this condition fails and 
+L[child] "y" >= d[parent/ "could be articulation node"] "x", then "could be articulation node" is an articulation point.
  
  
- leaf nodes can never be articulation points.
- Backedges are those edges which are missing in DFS tree (because of already visited through another path) and present in the graph.
- 
- For each node, compute two things:
- 1) Discovery Time : order in which we are visiting the nodes in DFS tree.
- 2) Low Time
+ TC: O(V+E)
 
 https://www.youtube.com/watch?v=qNVNoZJFp_g&list=PLFj4kIJmwGu3m30HfYDDufr3PZBfyngr0&index=10&ab_channel=FitCoder
 
 */
+
+
+
 
