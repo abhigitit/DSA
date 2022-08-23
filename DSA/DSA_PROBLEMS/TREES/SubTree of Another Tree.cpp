218e6709da2dcd50427876f1a62b12f9f9a8dedc
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-08-22 20:26:39 -0700

    Update SubTree of Another Tree.cpp

--- DSA/DSA_PROBLEMS/TREES/SubTree of Another Tree.cpp
+++ DSA/DSA_PROBLEMS/TREES/SubTree of Another Tree.cpp
@@ -1,5 +1,19 @@
 
 
+/*
+Leetcode: 572
+Intuition : If subroot is null, then return true because null tree is subtree of any tree.
+If subroot is present and root is null, return false; not possible.
+
+If they both are same tree, return true.
+Or
+Find the subroot in main tree's root's left part OR right part.
+
+
+Same tree Intuition: Both null trees are same, so return true as base case for both null trees.
+If values at both roots are same check fro both leftsubtree AND rightsubtree.
+TC : O(N)
+*/
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
