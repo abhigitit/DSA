ed4c2ecdf7ba53d7689f10ff55069c0bf97721a8
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-09-07 13:23:01 -0700

    Update Bellman Ford Algorithm.cpp

--- DSA/DSA_PROBLEMS/Graphs/Bellman Ford Algorithm.cpp
+++ DSA/DSA_PROBLEMS/Graphs/Bellman Ford Algorithm.cpp
@@ -29,9 +29,110 @@ After performing n-1 relaxations, it is guranteed that the distance array is pop
 Then, as the final step, perform one more relaxation and validate if the distance array is changing. If its changing, then we detect a negative edge wt cycle.
 
 https://www.youtube.com/watch?v=24HziTZ8_xo&t=9s&ab_channel=TECHDOSE
-
+TC: O(VE)
 */
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Graph
+{
+    int V;
+    vector<vector<int>> edges;
+    vector<int> parent;
+    vector<int> cost;
+    public:
+    Graph(int V,int e)
+    {
+        edges.resize(e);
+        parent.resize(V,0);
+        parent[0] = -1;
+        cost.resize(V,INT_MAX);
+        cost[0] = 0;
+        this->V = V;
+    }
+    
+    void addEdge(int edge, int v,int w, int wt)
+    {
+        edges[edge].push_back(v);
+        edges[edge].push_back(w);
+        edges[edge].push_back(wt);
+    }
+    
+    void Bellman()
+    {
+        bool updated;
+        for(int i=0;i<V-1;++i)
+        {
+            updated = false;
+            
+            for(int j=0;j<edges.size();j++)
+            {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                int wt = edges[j][2];
+                
+                if(cost[u]!=INT_MAX and cost[u]+wt < cost[v])
+                {
+  
+                    cost[v] = cost[u]+wt;
+                    parent[v] = u;
+                    updated = true;
+                }
+            }
+            if(!updated)
+            break;
+        }
+        
+         for(int j=0;j<edges.size() and updated==true;j++)
+            {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                int wt = edges[j][2];
+                
+                if(cost[u]!=INT_MAX and cost[u]+wt < cost[v])
+                {
+                   cout<<"Negative Edge Wt cycle detected"<<endl;
+                   break;
+                }
+            }
+        
+        if(!updated)
+        {
+            cout<<"Printing SPG"<<endl;
+            for(int i=1;i<V;++i)
+            {
+                
+               cout<<parent[i]<< "-->" <<i<<": "<<cost[i]<<endl; 
+            }
+        }
+        
+        
+    }
+    
+
+};
+
+
+int main()
+{
+    Graph g(5,10);
+    g.addEdge(0,0,1,6);
+    g.addEdge(1,0,2,7);
+    g.addEdge(2,1,2,8);
+    g.addEdge(3,2,3,9);
+    g.addEdge(4,2,4,-3);
+    g.addEdge(5,1,3,-4);
+    g.addEdge(6,3,4,7);
+    g.addEdge(7,1,4,5);
+    g.addEdge(8,4,1,-2);
+    g.addEdge(9,3,0,2);
+    
+g.Bellman();
+    return 0;
+}
 
 
 
