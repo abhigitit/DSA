9602501f0044be28552434e5f7967fdb6fd7b06a
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-09-07 12:13:40 -0700

    Update Dijkstras - Network Delay TIme.cpp

--- DSA/DSA_PROBLEMS/Graphs/Dijkstras - Network Delay TIme.cpp
+++ DSA/DSA_PROBLEMS/Graphs/Dijkstras - Network Delay TIme.cpp
@@ -11,43 +11,46 @@ TC: O(V+E)
 
 class Solution {
 public:
-    unordered_map<int,pair<int,int>> adj;
-     queue<pair<int,int>> BFS;
-    vector<int> distances;
+   
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        distances.resize(n+1,INT_MAX);
-       
+         unordered_map<int,vector<pair<int,int>>> adj;
+     queue<pair<int,int>> BFS;
+    vector<int> distances(n+1,INT_MAX);
+ 
         for(int i=0;i<times.size();++i)
         {
             int v = times[i][0];
             int w = times[i][1];
             int wt = times[i][2];
-            adj[v] = make_pair(w,wt);
-            adj[w] = make_pair(v,wt);
+            adj[v].push_back({w,wt});
         }
-        BFS.push({n,0});
-        distacnes[n] = 0;
+        BFS.push({k,0});
+        distances[k] = 0;
         
         while(!BFS.empty())
         {
-            pair<int,int> curr = BFS.top();
-            BFS.pop();
+            pair<int,int> curr = BFS.front();
+            BFS.pop(); 
             int curr_node = curr.first;
             int curr_dist = curr.second;
-            pair<int,int>::const_iterator it;
-            for(it=adj[curr_node].begin();it!=adj[curr_node].end();++it)
+           
+            for(auto it:adj[curr_node])
             {
-                int next_node = (*it).first;
-                int next_dist = (*it).second;
-                if(distance[next_node]>curr_dist+next_dist)
+               
+                int next_node = (it).first;
+                int next_dist = (it).second;
+                if(distances[next_node]>curr_dist+next_dist)
                 {
-                    distance[next_node] = curr_dist+next_dist;
-                    BFS.push({next_node,next_dist});
-                }
+                    BFS.push({next_node,next_dist+curr_dist});
+                    distances[next_node] = curr_dist+next_dist;
+                    
+                } 
+                
+               
             }
         }
         int res = 0;
-        for(int i=0;i<distances.size();++i)
+        for(int i=1;i<distances.size();++i)
         {
             if(distances[i]==INT_MAX)
                 return -1;
