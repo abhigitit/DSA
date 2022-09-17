9c07f9310ac527df6229ea90a6a154b5e6fbba0a
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-09-16 19:30:38 -0700

    Create Maximum Units on a Truck.cpp

---
+++ DSA/DSA_PROBLEMS/Arrays and Hashing/Maximum Units on a Truck.cpp
@@ -0,0 +1,42 @@
+/*
+
+Maximum Units on a Truck
+
+Intuition : Sort and use greedy approach
+
+O(NlogN)
+
+https://www.youtube.com/watch?v=qZUokf5dwYw&ab_channel=SehoLim
+*/
+
+
+bool compare(vector<int> &v1,vector<int> &v2)
+{
+    if(v1[1]>v2[1])
+        return true;
+    else
+        return false;
+}
+
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        
+        sort(boxTypes.begin(),boxTypes.end(),compare);
+        int res = 0;
+        int total = 0;
+        for(int i=0;i<boxTypes.size();++i)
+        {
+            if(truckSize<=boxTypes[i][0])
+            {
+                res+= truckSize*boxTypes[i][1]; 
+                break;
+            }
+            res+= boxTypes[i][0]*boxTypes[i][1];
+            truckSize-=boxTypes[i][0];
+            
+        }
+        
+        return res;
+    }
+};
