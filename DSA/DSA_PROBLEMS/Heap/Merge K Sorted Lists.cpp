d216354eafe6740e22c4516cb38a75968ed8426a
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-09-20 17:13:14 -0700

    Update Merge K Sorted Lists.cpp

--- DSA/DSA_PROBLEMS/Heap/Merge K Sorted Lists.cpp
+++ DSA/DSA_PROBLEMS/Heap/Merge K Sorted Lists.cpp
@@ -2,7 +2,7 @@
 
 Merge K Sorted Lists.cpp 
 Leetcode : 23
-
+and AE
 Intuition : Use min heap to store k elements each time.
 
 https://www.youtube.com/watch?v=kpCesr9VXDA&t=1194s&ab_channel=TECHDOSE
@@ -64,3 +64,43 @@ public:
         
     }
 };
+
+_______
+    
+ Vector of vectors
+ 
+ using namespace std;
+
+struct compare
+{
+bool operator()(vector<int>& a,vector<int>& b)
+{
+  return a[0]>b[0];
+}
+};
+vector<int> mergeSortedArrays(vector<vector<int>> arrays) {
+  int k = arrays.size();
+  vector<int> res;
+  
+  priority_queue<vector<int>, vector<vector<int>>, compare> heap;
+  for(int i=0;i<k;++i)
+    {
+     heap.push({arrays[i][0],i,0});
+    }
+
+ while(!heap.empty())
+   {
+     vector<int> temp = heap.top();
+     heap.pop();
+     int val = temp[0];
+     int vec_idx = temp[1];
+     int curr_idx = temp[2];
+     int next_idx = curr_idx+1;
+     res.push_back(val);
+     if(next_idx<arrays[vec_idx].size())
+     {
+       heap.push({arrays[vec_idx][next_idx], vec_idx,next_idx});
+     }
+   }
+  return res;
+}
