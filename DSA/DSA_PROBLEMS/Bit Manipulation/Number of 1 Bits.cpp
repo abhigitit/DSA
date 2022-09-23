3ef21554fbddb94d80fe524b5898bc1e4f235d81
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-09-22 17:55:24 -0700

    Create  Number of 1 Bits.cpp

---
+++ DSA/DSA_PROBLEMS/Bit Manipulation/Number of 1 Bits.cpp
@@ -0,0 +1,17 @@
+/*
+ Number of 1 Bits
+ 
+ */
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+       int res =0;
+        while(n)
+        {
+            res+=n&1;
+            n = (n>>1);
+        }
+        return res;
+    }
+};
