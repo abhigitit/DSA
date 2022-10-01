811998cd15c3a97417339ea72bb4a74d78c62b6f
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-09-30 19:41:17 -0700

    Update Find First and Last Position of Element in Sorted Array.cpp

--- DSA/DSA_PROBLEMS/Arrays and Hashing/Find First and Last Position of Element in Sorted Array.cpp
+++ DSA/DSA_PROBLEMS/Arrays and Hashing/Find First and Last Position of Element in Sorted Array.cpp
@@ -1,8 +1,63 @@
 /*
 Leetcode: 34
-
+Nick White
 */
 
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        vector<int> res(2,-1);
+        res[0] = findFirst(0,nums.size()-1,nums,target);
+        res[1] = findSecond(0,nums.size()-1,nums,target);
+        return res;
+    }
+    
+    int findFirst(int left, int right,vector<int>& nums, int target)
+    {
+        int idx = -1;
+        
+        while(left<=right)
+        {
+            int mid = left + (right-left)/2;
+            
+            if(nums[mid]>=target)
+                right=mid-1;
+            else
+                left=mid+1;
+            
+            if(nums[mid]==target)
+                idx = mid;
+        }
+        
+        
+        return idx;
+    }
+    
+    int findSecond(int left, int right,vector<int>& nums, int target)
+    {
+        int idx =-1;
+        
+         while(left<=right)
+        {
+         int mid = left + (right-left)/2;
+            
+            if(nums[mid]<=target)
+                left=mid+1;
+            else
+                right=mid-1;
+            
+            if(nums[mid]==target)
+                idx = mid;
+         }
+        return idx;
+    }
+};
+
+
+Brute force
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
