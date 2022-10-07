13463dc5e649c88ac723ad94f6855bafa98fb0ca
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-10-07 09:03:18 -0700

    Update Shortest Unsorted Continuous Subarray.cpp

--- DSA/DSA_PROBLEMS/Arrays and Hashing/Shortest Unsorted Continuous Subarray.cpp
+++ DSA/DSA_PROBLEMS/Arrays and Hashing/Shortest Unsorted Continuous Subarray.cpp
@@ -1,9 +1,10 @@
 /*
 Shortest Unsorted Continuous Subarray leetcode 581
 
-Intuition 1: Record indices before sorting and collect min and max index where num is not in right position.
+Intuition 1: Record indices before sorting and collect min and max index where num is not in right position. O(nlogn)
 
-Intuition 2:
+Intuition 2: Find minimum number of unsorted part, find max number of unsorted part and check the indices where they fit in. O(n)
+
 */
 
 
@@ -42,3 +43,38 @@ public:
 };
 
 2:
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int low = INT_MAX;
+        int high = INT_MIN;
+        
+        for(int i=1;i<nums.size();++i)
+        {
+            if(nums[i]<nums[i-1])
+                low = min(low,nums[i]);
+        }
+        
+        
+         for(int i=nums.size()-2;i>=0;--i)
+        {
+            if(nums[i]>nums[i+1])
+                high = max(high,nums[i]);
+        }
+        
+        int l,r;
+        for(l=0;l<nums.size();++l)
+        {
+            if(low<nums[l])
+                break;
+        }
+        
+        for(r=nums.size()-1;r>=0;--r)
+        {
+            if(high>nums[r])
+                break;
+        }
+        
+        return r-l<0 ? 0 : r-l+1;
+    }
+};
