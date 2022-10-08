13151c2a7d41428614c18391b7b42a89ac63adfc
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-10-07 17:45:02 -0700

    Create Search in Rotated Sorted Array.cpp

---
+++ DSA/DSA_PROBLEMS/Binary Search/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,47 @@
+/*
+Search in Rotated Sorted Array
+
+Intuition: There are 2 sorted parts in roated sorted array. Find the minimum element to find in which sorted part the target lies.
+Then apply binary search
+
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left<right)
+        {
+            int midpoint = left + (right-left)/2;
+            if(nums[midpoint]>nums[right])
+                left = midpoint + 1;
+            else
+                right = midpoint;
+        }
+        int start = left;
+        left = 0;
+        right = nums.size() - 1;
+        
+        
+            if(target>=nums[start] && target<=nums[right]) 
+                left = start;
+        else
+            right = start;
+        
+      while(left<=right)
+        {
+            int midpoint = left + (right-left)/2;
+          if(nums[midpoint]==target)
+              return midpoint;
+            else if(nums[midpoint]<target)
+                left = midpoint + 1;
+            else
+                right = midpoint-1;
+        }
+       
+       return -1;
+        
+    }       
+    
+};
