393ef1a36f2553be01bf2c17281ffe95d8014b23
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-10-07 17:36:01 -0700

    Create  Find Minimum in Rotated Sorted Array.cpp

---
+++ DSA/DSA_PROBLEMS/Binary Search/Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,23 @@
+/*
+ Find Minimum in Rotated Sorted Array
+ Intuition: The element which is lesses than the previous one is the minimum. O(N)
+ To do in logN, eliminate the side that is sorted.
+ */
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left<right)
+        {
+            int midpoint = left + (right-left)/2;
+            if(nums[midpoint]>nums[right])
+                left = midpoint + 1;
+            else
+                right = midpoint;
+        }
+        return nums[left];
+
+    }
+};
