d078ce6927ca54830563ea9048df76f3837b64c4
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-10-10 12:10:44 -0700

    Update Find Median from Data Stream.cpp

--- DSA/DSA_PROBLEMS/Heap/Find Median from Data Stream.cpp
+++ DSA/DSA_PROBLEMS/Heap/Find Median from Data Stream.cpp
@@ -40,6 +40,73 @@ How to handle insertions into heaps : https://www.youtube.com/watch?v=1LkOrc-Le-
 
 */
 
-
+class MedianFinder {
+public:
+    priority_queue<int> max_heap;
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        int lsize = max_heap.size();
+        int rsize = min_heap.size();
+        
+        if(lsize==0)
+            max_heap.push(num);
+        
+        else if(lsize==rsize)
+        {
+            if(num<min_heap.top())
+                max_heap.push(num);
+            else
+            {
+                int temp = min_heap.top();
+                min_heap.pop();
+                max_heap.push(temp);
+                min_heap.push(num);
+            }
+        }
+        
+        else
+        {
+            if(rsize==0)
+            {
+                if(num>max_heap.top())
+                    min_heap.push(num);
+                else
+                {
+                    int temp = max_heap.top();
+                    max_heap.pop();
+                    min_heap.push(temp);
+                    max_heap.push(num);
+                }
+            }
+            else if(num>=max_heap.top())
+                min_heap.push(num);
+            else
+            {
+                if(num<max_heap.top())
+                {
+                    int temp = max_heap.top();
+                    max_heap.pop();
+                    max_heap.push(num);
+                    min_heap.push(temp);
+                }
+                else
+                    min_heap.push(num);
+            }
+        }
+    }
+    
+    double findMedian() {
+       int lsize = max_heap.size();
+        int rsize = min_heap.size();
+        if(lsize>rsize)
+            return double(max_heap.top());
+        else
+            return (double(max_heap.top())+double(min_heap.top()))/2;
+    }
+};
 
 
