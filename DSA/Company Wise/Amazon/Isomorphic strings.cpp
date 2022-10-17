53f8176aa27f7200055c71480274efcbbc1cb267
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-10-17 12:40:31 -0700

    Create Isomorphic strings.cpp

---
+++ DSA/Company Wise/Amazon/Isomorphic strings.cpp
@@ -0,0 +1,28 @@
+/*
+Isomorphic strings
+https://www.youtube.com/watch?v=7yF-U1hLEqQ&ab_channel=NeetCode
+
+We cant have one char mapping to two different and also two different mapping to same char.
+*/
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        return helper(s,t) && helper(t,s);
+    }
+    
+    bool helper(string s, string t)
+    {
+        map<char,char> cache;
+        for(int i=0;i<s.size();++i)
+        {
+            if(cache.count(s[i]))
+            {
+                if(cache[s[i]]!=t[i]) return false;
+            }
+            else
+                cache[s[i]]=t[i];
+        }
+        return true;
+    }
+};
