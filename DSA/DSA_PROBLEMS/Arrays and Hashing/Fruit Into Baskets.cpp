1d76ae1c8a3c42b666d556629fbb62ab67bf5f67
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-11-18 19:25:21 -0800

    Update Fruit Into Baskets.cpp

--- DSA/DSA_PROBLEMS/Arrays and Hashing/Fruit Into Baskets.cpp
+++ DSA/DSA_PROBLEMS/Arrays and Hashing/Fruit Into Baskets.cpp
@@ -4,9 +4,13 @@ Fruit Into Baskets
 https://www.youtube.com/watch?v=s_zu2dOkq80&ab_channel=NickWhite
 
 Longest Contiguos Subarray of only two numbers
+
+exaplination of else statement : curr_max = last_fruit_count+1;
+3,3,3,1,2...when on 2, we have seen a new fruit other than 3,1 so currmax = lastfruit(1) count + 1(new fruit 2).
 */
 
-class Solution {
+
+ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int last_fruit = -1;
@@ -28,14 +32,12 @@ public:
             if(fruit == last_fruit)
                 last_fruit_count++;
             else
-                last_fruit_count=1;
-            
-            if(fruit != last_fruit)
             {
                 second_last_fruit = last_fruit;
                  last_fruit=fruit;
-                
+                  last_fruit_count=1;
             }
+
 
             res = max(res,curr_max);
         }
