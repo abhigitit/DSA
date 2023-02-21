dc29430a7c6274359951bc8bf276c69634561836
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-02-21 20:44:35 +0530

    Update Shortest Path to Get Food.cpp

--- DSA/DSA_PROBLEMS/Graphs 1/Shortest Path to Get Food.cpp
+++ DSA/DSA_PROBLEMS/Graphs 1/Shortest Path to Get Food.cpp
@@ -7,6 +7,64 @@ BFS
 class Solution {
 public:
     int getFood(vector<vector<char>>& grid) {
+        queue<vector<int>> que;
+        set<pair<int,int>> visited;
+        
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        vector<vector<int>> directions = {{-1,0},{1,0},{0,1},{0,-1}};
+        for(int i=0;i<rows;++i)
+        {
+            for(int j=0;j<cols;++j)
+            {
+                if(grid[i][j]=='*')
+                {
+                    que.push({i,j,0});
+                    visited.insert({i,j});
+                }
+                    
+            }
+        }
+        
+        while(!que.empty())
+        {
+            auto temp = que.front();
+            que.pop();
+            
+            int steps = temp[2];
+            int x = temp[0];
+            int y = temp[1];
+            
+            if(grid[x][y]=='#')
+                return steps;
+            
+            for(int i=0;i<directions.size();++i)
+            {
+                int dx = x + directions[i][0];
+                int dy = y + directions[i][1];
+                
+                if(min(dx,dy)<0 or dx>=rows or dy>=cols or visited.count({dx,dy}) or grid[dx][dy]=='X')
+                    continue;
+                
+                
+                que.push({dx,dy,steps+1});
+                visited.insert({dx,dy});
+                
+                
+            }
+            
+        }
+        return -1;
+    }
+};
+
+
+______
+
+class Solution {
+public:
+    int getFood(vector<vector<char>>& grid) {
 
         queue<vector<int>> BFS;
         for(int i=0;i<grid.size();++i)
