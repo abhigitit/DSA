0916b5ec4b9232e20e28f3e86f537cf4f6e12322
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-02-21 20:52:14 +0530

    Update Shortest Path in Binary Matrix.cpp

--- DSA/DSA_PROBLEMS/Graphs 1/Shortest Path in Binary Matrix.cpp
+++ DSA/DSA_PROBLEMS/Graphs 1/Shortest Path in Binary Matrix.cpp
@@ -11,7 +11,7 @@ Shortest Path in Binary Matrix
 
 TC : O(n*m)
 */
-
+Incrementing len after each layer exploration
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
@@ -57,6 +57,55 @@ public:
             
             
             len++;
+        }      
+        return -1;
+    }
+};
+
+_____
+   Incrementing len with every node with value 0
+  
+  class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        
+        if(grid[0][0]==1) return -1;
+        queue<vector<int>> bfs;
+        set<pair<int,int>> visited;
+        visited.insert({0,0});
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        bfs.push({0,0,1});
+        vector<vector<int>> directions = {{1,0},{-1,0},{0,-1},{0,1},{1,1},{-1,1},{1,-1},{-1,-1}};
+        
+        while(!bfs.empty())
+        {
+            
+               auto temp = bfs.front();
+               bfs.pop();
+            
+            int x = temp[0];
+            int y = temp[1];
+            int len = temp[2];
+
+            if(x==rows-1 and y==cols-1)
+                return len;
+            for(int j=0;j<directions.size();++j)
+            {
+                int new_x = x + directions[j][0];
+                int new_y = y + directions[j][1];
+
+                if(min(new_x,new_y) < 0 or new_x == rows or new_y == cols or visited.count({new_x,new_y}) or grid[new_x][new_y]==1)
+                {
+                    continue;
+                }
+                    
+                  bfs.push({new_x,new_y,len+1});
+                  visited.insert({new_x,new_y});
+                
+            } 
+  
         }      
         return -1;
     }
