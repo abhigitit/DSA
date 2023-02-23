ccadf10d47851c6c3e4c8ccda3360337a51fbb9a
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-02-23 22:03:00 +0530

    Create Shortest Bridge.cpp

---
+++ DSA/DSA_PROBLEMS/Graphs 1/Shortest Bridge.cpp
@@ -0,0 +1,11 @@
+/*
+Shortest Bridge
+
+Intuition : Shortest distance between 2 islands. To collect the 1s in a single island, we apply dfs. Then with those coordinates, we apply multi source, multi
+destination BFS to reach second islands' 1's.
+For shortest path with no weights = BFS
+
+O(N*M)
+
+*/
+
