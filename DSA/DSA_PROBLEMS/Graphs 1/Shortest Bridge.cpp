48879c5d6962b6cafa38acd38b1281196b5f9753
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-02-23 22:06:54 +0530

    Update Shortest Bridge.cpp

--- DSA/DSA_PROBLEMS/Graphs 1/Shortest Bridge.cpp
+++ DSA/DSA_PROBLEMS/Graphs 1/Shortest Bridge.cpp
@@ -5,6 +5,8 @@ Intuition : Shortest distance between 2 islands. To collect the 1s in a single i
 destination BFS to reach second islands' 1's.
 For shortest path with no weights = BFS
 
+As soon as the dfs finds the first island and returns we need to call bfs and return the shortest dis
+
 O(N*M)
 
 */
