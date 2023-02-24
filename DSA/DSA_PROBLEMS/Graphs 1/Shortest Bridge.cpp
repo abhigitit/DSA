8d7c5fa21a9f51fdf0d7a4df669cad99de6b825b
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-02-24 08:34:50 +0530

    Update Shortest Bridge.cpp

--- DSA/DSA_PROBLEMS/Graphs 1/Shortest Bridge.cpp
+++ DSA/DSA_PROBLEMS/Graphs 1/Shortest Bridge.cpp
@@ -11,3 +11,85 @@ O(N*M)
 
 */
 
+
+class Solution {
+
+int rows;
+int cols;
+set<int> visited;
+queue<pair<int,int>> q;
+
+vector<vector<int>> directions;
+   bool invalid(int i, int j,vector<vector<int>>& grid)
+    {
+        return (min(i,j)<0 or i>=rows or j>=cols or visited.count(i*cols + j));
+        
+    }
+
+    void dfs(int i, int j, vector<vector<int>>& grid)
+    {
+        if(invalid(i,j,grid) or !grid[i][j])
+            return;
+        visited.insert(i*cols + j);
+        q.push({i,j});
+        dfs(i+1,j, grid);
+        dfs(i-1,j, grid);
+        dfs(i,j+1, grid);
+        dfs(i,j-1, grid);
+        
+
+    }
+
+    int bfs(vector<vector<int>>& grid)
+    {
+        int res = 0;
+        while(!q.empty())
+        {
+            int s = q.size();
+            for(int i=0;i<s;++i)
+            {
+                auto temp = q.front();
+                q.pop();
+                int x = temp.first;
+                int y = temp.second;
+
+                for(int k=0;k<directions.size();++k)
+                {
+                    int dx = x + directions[k][0];
+                    int dy = y + directions[k][1];
+
+                    if(invalid(dx,dy,grid))
+                        continue;
+                    if(grid[dx][dy])
+                        return res;
+                    q.push({dx,dy});
+                    visited.insert(dx*cols + dy);
+                }
+            }
+            res++;
+        }
+        return -1;
+
+    }
+public:
+    int shortestBridge(vector<vector<int>>& grid) {
+        rows = grid.size();
+        cols = grid[0].size();
+        directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        
+        for(int i=0;i<rows;++i)
+        {
+            for(int j=0;j<cols;++j)
+            {
+                if(grid[i][j])
+                {
+                    dfs(i,j,grid);
+                    
+                    return bfs(grid);
+                }
+            }
+        }
+        return 0;
+    }
+   
+};
