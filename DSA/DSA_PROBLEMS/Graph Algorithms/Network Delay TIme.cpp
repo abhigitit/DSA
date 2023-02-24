7fb69ca31b1468818a601da9fe3d6f91d0002010
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-02-24 23:22:54 +0530

    Update Network Delay TIme.cpp

--- DSA/DSA_PROBLEMS/Graph Algorithms/Network Delay TIme.cpp
+++ DSA/DSA_PROBLEMS/Graph Algorithms/Network Delay TIme.cpp
@@ -5,59 +5,58 @@ Leetcode: 743
 Intuiton : Apply Dijkstra's algorithm to find out minimum time to reach the farthest node.
 A simple BFS solution.
 
+Solution is exact to algorithm code. If all nodes are node reachable i.e. at the end, if the hashmap shortest's size != no of nodes, return -1
+
 TC: O(V+E)
 */
 
 
+typedef pair<int,int> P;
 class Solution {
 public:
-   
+    unordered_map<int,vector<pair<int,int>>> adj;
+    unordered_map<int,int> shortest;
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-         unordered_map<int,vector<pair<int,int>>> adj;
-     queue<pair<int,int>> BFS;
-    vector<int> distances(n+1,INT_MAX);
- 
+        priority_queue<P, vector<P>, greater<P>> minHeap;
+
         for(int i=0;i<times.size();++i)
         {
             int v = times[i][0];
             int w = times[i][1];
-            int wt = times[i][2];
-            adj[v].push_back({w,wt});
+            int cost = times[i][2];
+
+            adj[v].push_back({w,cost});
         }
-        BFS.push({k,0});
-        distances[k] = 0;
-        
-        while(!BFS.empty())
-        {
-            pair<int,int> curr = BFS.front();
-            BFS.pop(); 
-            int curr_node = curr.first;
-            int curr_dist = curr.second;
-           
-            for(auto it:adj[curr_node])
-            {
-               
-                int next_node = (it).first;
-                int next_dist = (it).second;
-                if(distances[next_node]>curr_dist+next_dist)
-                {
-                    BFS.push({next_node,next_dist+curr_dist});
-                    distances[next_node] = curr_dist+next_dist;
-                    
-                } 
-                
-               
-            }
-        }
+
+        minHeap.push({0,k});
         int res = 0;
-        for(int i=1;i<distances.size();++i)
+        while(!minHeap.empty() and shortest.size()!=n)
         {
-            if(distances[i]==INT_MAX)
-                return -1;
-            else
-                res = max(res,distances[i]);
+            
+            auto curr_node_info = minHeap.top();
+            minHeap.pop();
+            
+            int cost_till_curr = curr_node_info.first;
+            int curr_node = curr_node_info.second;
+            
+            if(shortest.count(curr_node))
+                continue;
+                
+            shortest[curr_node] = cost_till_curr;
+            res = max(res,cost_till_curr);
+            
+            for(auto v: adj[curr_node])
+            {
+                int next_node = v.first;
+                int cost_to_next = v.second;
+                if(!shortest.count(next_node))
+                    minHeap.push({(cost_to_next + cost_till_curr), next_node});
+                    
+            }
+            
         }
-        return res;
-        
+
+        return shortest.size()==n ? res : -1;
+
     }
 };
