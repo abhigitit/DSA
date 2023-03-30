83a86cbe561e0723b213227d694c2c8ebb2c3648
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-03-30 08:52:27 -0700

    Create Check If a String Contains All Binary Codes of Size K.cpp

---
+++ DSA/DSA_PROBLEMS/Arrays and Hashing - Sliding Window and Two Pointer/Check If a String Contains All Binary Codes of Size K.cpp
@@ -0,0 +1,15 @@
+/*
+Check If a String Contains All Binary Codes of Size K
+*/
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        if(k>s.size())
+            return false;
+        unordered_set<string> res;
+        for(int i =0;i<= s.size()-k;i++)
+            res.insert(s.substr(i,k));
+
+        return res.size()==pow(2,k);
+    }
+};
