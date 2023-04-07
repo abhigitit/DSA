159f65b48f0fe3eafa0bc0396b0e291b7670e529
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-04-07 12:59:15 -0700

    Update Network Delay TIme.cpp

--- DSA/DSA_PROBLEMS/Graphs 2/Network Delay TIme.cpp
+++ DSA/DSA_PROBLEMS/Graphs 2/Network Delay TIme.cpp
@@ -13,50 +13,56 @@ TC: O(V+E)
 
 typedef pair<int,int> P;
 class Solution {
-public:
-    unordered_map<int,vector<pair<int,int>>> adj;
-    unordered_map<int,int> shortest;
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        priority_queue<P, vector<P>, greater<P>> minHeap;
+    unordered_map<int,vector<P>> adj;
+    unordered_map<int,int> time;
+    priority_queue<P,vector<P>, greater<P>> minheap;
 
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         for(int i=0;i<times.size();++i)
         {
-            int v = times[i][0];
-            int w = times[i][1];
+            int u = times[i][0];
+            int v = times[i][1];
             int cost = times[i][2];
-
-            adj[v].push_back({w,cost});
+            adj[u].push_back({v,cost});
         }
 
-        minHeap.push({0,k});
-        int res = 0;
-        while(!minHeap.empty() and shortest.size()!=n)
+    minheap.push({0,k});
+    
+
+    while(!minheap.empty() and time.size()!=n)
+    {
+        auto nodeInfo = minheap.top();
+        minheap.pop();
+        int node = nodeInfo.second;
+        int timeNode = nodeInfo.first;
+
+        if(time.count(node))
+            continue;
+        
+        time[node] = timeNode;
+
+        for(auto it:adj[node])
         {
-            
-            auto curr_node_info = minHeap.top();
-            minHeap.pop();
-            
-            int cost_till_curr = curr_node_info.first;
-            int curr_node = curr_node_info.second;
-            
-            if(shortest.count(curr_node))
-                continue;
-                
-            shortest[curr_node] = cost_till_curr;
-            res = max(res,cost_till_curr);
-            
-            for(auto v: adj[curr_node])
+            int nei = it.first;
+            int timeNei = it.second;
+            if(!time.count(nei))
             {
-                int next_node = v.first;
-                int cost_to_next = v.second;
-                if(!shortest.count(next_node))
-                    minHeap.push({(cost_to_next + cost_till_curr), next_node});
-                    
+                minheap.push({timeNode+timeNei,nei});
             }
-            
         }
 
-        return shortest.size()==n ? res : -1;
+    }
 
+
+    int res = INT_MIN;
+
+    for(auto it:time)
+    {
+        res = max(res,it.second);
+    }
+
+    return time.size()==n ? res : -1;
+    
     }
 };
