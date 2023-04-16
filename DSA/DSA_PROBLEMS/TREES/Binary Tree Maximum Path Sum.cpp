2e18f19fc2d5872cb555e2f0c5950f2bea9debdd
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-04-15 22:21:29 -0700

    Update Binary Tree Maximum Path Sum.cpp

--- DSA/DSA_PROBLEMS/TREES/Binary Tree Maximum Path Sum.cpp
+++ DSA/DSA_PROBLEMS/TREES/Binary Tree Maximum Path Sum.cpp
@@ -1,9 +1,7 @@
 /*
 
 Leetcode: Binary Tree Maximum Path Sum
-https://www.youtube.com/watch?v=cSnETAcziS0&ab_channel=GeeksforGeeks
-Intution : At every root, the max value can be root->val or max(left child value , right child val) + root->val
-At top root, both right and left paths can be considered so maxAtTopRoot = max(maxAtRoot,l+r+root->val);
+https://www.youtube.com/watch?v=JBYs5J4skZE
 
 TC:O(N)
 
@@ -11,22 +9,22 @@ TC:O(N)
 
 class Solution {
 public:
-    int res;
     int maxPathSum(TreeNode* root) {
-        res = INT_MIN;
-        helper(root);
-        return res;
+        int ans = INT_MIN;
+        helper(root,ans);
+        return ans;
     }
-    
-    int helper(TreeNode* root)
+
+    int helper(TreeNode* root, int& ans)
     {
         if(!root) return 0;
-        
-        int l = helper(root->left);
-        int r = helper(root->right);
-        int maxAtRoot = max(root->val,max(l,r)+root->val);
-        int maxAtTopRoot = max(maxAtRoot,l+r+root->val);
-        res = max(res,maxAtTopRoot);
-        return maxAtRoot;
+
+        int left = helper(root->left,ans);
+        int right = helper(root->right,ans);
+
+        int straightPath = max(root->val , max(root->val+left,root->val+right));
+        int curvedPath = left + right + root->val;
+        ans = max(ans,max(straightPath,curvedPath));
+        return straightPath;
     }
 };
