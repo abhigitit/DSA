605e85e30f53b9c9e51567b4644a1c46fd0f7b9e
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-04-16 20:57:48 -0700

    Update Detect Cycle in Undirected Graph.cpp

--- DSA/DSA_PROBLEMS/Graph Algorithms/Detect Cycle in Undirected Graph.cpp
+++ DSA/DSA_PROBLEMS/Graph Algorithms/Detect Cycle in Undirected Graph.cpp
@@ -4,3 +4,29 @@ Detect Cycle in Undirected Graph.cpp
 
 */
 
+bool Graph::isCyclicUtil(int v, bool visited[], int parent)
+{
+ 
+    // Mark the current node as visited
+    visited[v] = true;
+ 
+    // Recur for all the vertices
+    // adjacent to this vertex
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i) {
+ 
+        // If an adjacent vertex is not visited,
+        // then recur for that adjacent
+        if (!visited[*i]) {
+            if (isCyclicUtil(*i, visited, v))
+                return true;
+        }
+ 
+        // If an adjacent vertex is visited and
+        // is not parent of current vertex,
+        // then there exists a cycle in the graph.
+        else if (*i != parent)
+            return true;
+    }
+    return false;
+}
