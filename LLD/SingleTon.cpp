e9db8131cb52276013fc73b2f205eb1f7a683dcf
abhigitit <90536339+abhigitit@users.noreply.github.com>
2023-05-30 16:53:32 -0700

    Update SingleTon.cpp

--- LLD/SingleTon.cpp
+++ LLD/SingleTon.cpp
@@ -11,6 +11,12 @@ Resource Sharing: By having only one instance of a class, you can share resource
 For example, if you have a database connection or a configuration manager, having multiple instances could lead to unnecessary resource consumption or conflicts. The Singleton pattern ensures that all code accesses the same instance, allowing efficient sharing of resources.
 Global Access: The Singleton pattern provides a convenient way to access an object from anywhere in the application without passing it around as a parameter.
 This can be particularly useful for objects that need to be accessed by various components or modules of a system.
+
+It is not thread safe. If multiple threads at the same time try to create instances, the whole purpose is destroyed.
+
+Use mutex locks.
+
+
 https://www.youtube.com/watch?v=oeQWxhlnCHM
 
 */
